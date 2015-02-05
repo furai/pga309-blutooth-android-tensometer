e145b449e61ec538abb6c63756c6c7cd4943871c
Furai <furai@thd.vg>
2015-02-05 15:33:44 +0100

    [added] new EEPROM arduino sketch

---
+++ arduino_sketches/EEPROM/testWriteRead/testWriteRead.ino
@@ -0,0 +1,43 @@
+#include <Wire.h> 
+
+#define chip 0x50
+ 
+void loop(){}
+ 
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+ 
+  delay(5);
+}
+ 
+byte readEEPROM(int deviceaddress, unsigned int eeaddress ) 
+{
+  byte rdata = 0xFF;
+ 
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+ 
+  Wire.requestFrom(deviceaddress,1);
+ 
+  if (Wire.available()) rdata = Wire.read();
+ 
+  return rdata;
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();  
+ 
+  unsigned int address = 0;
+ 
+  writeEEPROM(chip, address, 123);
+  Serial.print(readEEPROM(chip, address), DEC);
+}

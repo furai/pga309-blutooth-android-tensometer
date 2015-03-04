31c11391306b7d841e2b81f9127e8a9dad485187
Furai <furai@thd.vg>
2015-03-04 15:54:48 +0100

    [added] basic logic to the PGA309 test pin programming, a lot left to do

--- arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
+++ arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
@@ -1,4 +1,10 @@
+#include <Wire.h>
+#define PGA309Addr 0x40
+
 int testPin = 52;
+
+void writePGA309Register(unsigned int addr, int rgstr,  int data);
+int readPGA309Register(unsigned int addr, int rgstr);
 
 void setup(){
   Serial.begin(9600);
@@ -11,3 +17,17 @@ void setup(){
 void loop(){
 }
   
+void writePGA309Register(unsigned int addr, int rgstr,  int data){
+  Wire.beginTransmission(addr);
+  Wire.write(rgstr);
+}
+
+int readPGA309Register(unsigned int addr, int rgstr){
+  int recData
+  Wire.beginTransmission(addr);
+  Wire.write(rgstr);
+  Wire.endTransmission(); 
+  Wire.requestFrom(addr,1);
+  if (Wire.available()) recData = Wire.receive();
+  return recData;
+}

86bddd902c1fa70aa179fdb35ae9595ce9d825ce
Furai <furai@thd.vg>
2015-03-04 13:51:28 +0100

    [modified] testPin_I2C.ino

--- arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
+++ arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
@@ -1,26 +1,11 @@
-#include <SendOnlySoftwareSerial.h>
-
 int testPin = 52;
-int prgPin = 48;
-
-SendOnlySoftwareSerial PGA309UART (prgPin);  
 
 void setup(){
   Serial.begin(9600);
-  PGA309UART.begin(9600);
-  pinMode(testPin, OUTPUT);
-  pinMode(prgPin, OUTPUT);
   //Setting the test pin high enables direct writing of internal registers and stops transactions with External EEPROM
+  pinMode(testPin, OUTPUT);
   digitalWrite(testPin, HIGH);
-  //The PRG pin should always be high when no communication is in progress
-  digitalWrite(prgPin, HIGH);
-  delay(50);
-  //Setting up the external EEPROM pointer address
-  //Sending initialisation byte 0x55  
-  PGA309UART.write(0x55);
-  PGA309UART.write(0x02);
-  PGA309UART.write();
-  PGA309UART.write();
+  delay(20);
 }
 
 void loop(){

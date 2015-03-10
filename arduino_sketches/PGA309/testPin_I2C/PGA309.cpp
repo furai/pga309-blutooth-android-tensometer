83a26d2189c983f0a01b5856559184467cf1088a
Furai <furai@thd.vg>
2015-03-10 16:48:19 +0100

    [added] missing files to VCS

---
+++ arduino_sketches/PGA309/testPin_I2C/PGA309.cpp
@@ -0,0 +1,75 @@
+/*
+	PGA309.h - Library for communicating with PGA309.
+	Created by Lucas Grzegorczyk, 05.03.2015
+*/
+
+#include "Arduino.h"
+#include "PGA309.h"
+
+/*	Function: writePGA309Register
+		Writes 2 bytes of data to PGA309 register.
+	Parameters:
+		rgstr - register number (int)
+		data - data to be written to the register (int)
+	Returns:
+		Integer - when different than 0 indicates error.
+*/
+int PGA309::writePGA309Register(int rgstr,  int data) {
+	Wire.beginTransmission(PGA309Addr);
+	Wire.write(rgstr);
+	Wire.write(data >> 8);
+	Wire.write(data & 0xFF);
+	return Wire.endTransmission();   
+}
+
+/*	Function: readPGA309Register
+		Reads 2 bytes of data from PGA309 register.
+	Parameters:
+		rgstr - register number (int)
+		precData - pointer for the array of bytes
+	Returns:
+		Integer - when positive indicates success, when negative - means that there was error.
+		Modified arrray.
+*/
+int PGA309::readPGA309Register(int rgstr, byte precData[]) {
+	int error;
+	Wire.beginTransmission(PGA309Addr);
+	Wire.write(rgstr);
+	if((error = Wire.endTransmission()) != 0)
+		return -error;
+	Wire.requestFrom(PGA309Addr, 2);
+	int i = 0;
+
+	while (Wire.available()){
+		precData[i] = Wire.read();
+		i++;
+	}
+}
+
+/*	Function: print_binary
+		Prints number in binary format with leading zeros.
+	Parameters:
+		v - number (int)
+		num_places - number of places to display (int)
+*/
+String PGA309::binaryFormat(int v, int numOfPlaces){
+	int mask=0, n;
+	String str = "";
+
+	for (n=1; n<=num_places; n++){
+		mask = (mask << 1) | 0x0001;
+	}
+
+	v = v & mask;  
+
+	while(num_places){
+		if (v & (0x0001 << num_places-1)){
+			str += '1';
+		} else {
+			str += '0';
+		}
+
+		--num_places;
+	}
+	return str;
+}
---
+++ arduino_sketches/PGA309/testPin_I2C/PGA309.h
@@ -0,0 +1,27 @@
+/*
+	PGA309.h - Library for communicating with PGA309.
+	Created by Lucas Grzegorczyk, 05.03.2015
+*/
+
+#ifndef PGA309_h
+#define PGA309_h
+
+#define testPin 0x34
+
+#include "Arduino.h"
+
+class PGA309
+{
+	public:
+		PGA309(int address);
+		void enableTestPin();
+		void disableTestPin();
+		int writePGA309Register(int rgstr,  int data);
+		int readPGA309Register(int rgstr, byte precData[]);
+		String binaryFormat(int n, int numOfPlaces);
+	private:
+		int _address;
+		bool testPinEnabled;
+};
+
+#endif

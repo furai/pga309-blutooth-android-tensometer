316756ae26e27e132841f1a380f207b05c2df0ee
Furai <furai@thd.vg>
2015-03-10 21:09:16 +0100

    [added] test of the library

--- arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
+++ arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
@@ -10,28 +10,24 @@ void setup() {
 	Wire.begin();
 	Serial.println(pga.getAddress());
 	byte* data;
-	data = pga.getRecData();
-	for (int i = 0; i < 2; i++){
-		Serial.print(pga.binaryFormat(data[i],8));
-		Serial.print(" ");
+	
+	//Setting the test pin high enables direct writing of internal registers and stops transactions with External EEPROM
+	pga.enableTestPin();
+	Serial.print("Status of writing to the PGA309 internal register: ");
+	Serial.println(pga.writePGA309Register(0x03, 0x1248));
+	int testdata;
+	testdata = pga.readPGA309Register(0x03);
+	if (testdata != 0){
+		Serial.print("There was error while reading: ");
+		Serial.println(testdata);
+	} else {
+		Serial.print("Successfully read data from the register. Data: ");
+		data = pga.getRecData();
+		for (int i = 0; i < 2; i++){
+			Serial.print(pga.binaryFormat(data[i],8));
+			Serial.print(" ");
+		}
 	}
-//Setting the test pin high enables direct writing of internal registers and stops transactions with External EEPROM
-	// pinMode(testPin, OUTPUT);
-	// digitalWrite(testPin, HIGH);
-	// Serial.print("Status of writing to the PGA309 internal register: ");
-	// Serial.println(writePGA309Register(0x02, 0x1248));
-	// int testdata;
-	// testdata = readPGA309Register(0x02, recData);
-	// if (testdata < 0){
-	// 	Serial.print("There was error while reading: ");
-	// 	Serial.println(-testdata);
-	// } else {
-	// 	Serial.print("Successfully read data from the register. Data: ");
-	// 	for (int i = 0; i < 2; i++){
-	// 		print_binary(recData[i],8);
-	// 		Serial.print(" ");
-	// 	}
-	// }
 }
 
 void loop() {

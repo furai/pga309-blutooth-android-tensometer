218de11395059e156e8713a4d58b038740b04dfe
Furai <furai@thd.vg>
2015-03-10 23:24:30 +0100

    [added] dirty logic to test PGA309

--- arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
+++ arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
@@ -2,19 +2,28 @@
 #include "PGA309.h"
 
 #define PGA309ADDR 0x40
+#define REG3 0x0500
+#define REG4 0x8000
+#define REG6 0x0600
 
 PGA309 pga(PGA309ADDR);
 
 void setup() {
 	Serial.begin(9600);
 	Wire.begin();
-	Serial.println(pga.getAddress());
 	byte* data;
 	
 	//Setting the test pin high enables direct writing of internal registers and stops transactions with External EEPROM
 	pga.enableTestPin();
-	Serial.print("Status of writing to the PGA309 internal register: ");
-	Serial.println(pga.writePGA309Register(0x03, 0x1248));
+	Serial.print("Status of writing to the PGA309 internal register 3: ");
+	Serial.println(pga.writePGA309Register(0x03, REG3));
+	delay(10);
+	Serial.print("Status of writing to the PGA309 internal register 4: ");
+	Serial.println(pga.writePGA309Register(0x04, REG4));
+	delay(10);
+	Serial.print("Status of writing to the PGA309 internal register 6: ");
+	Serial.println(pga.writePGA309Register(0x06, REG6));
+	delay(10);
 	int testdata;
 	testdata = pga.readPGA309Register(0x03);
 	if (testdata != 0){
@@ -27,7 +36,35 @@ void setup() {
 			Serial.print(pga.binaryFormat(data[i],8));
 			Serial.print(" ");
 		}
+		Serial.println();
 	}
+	testdata = pga.readPGA309Register(0x04);
+	if (testdata != 0){
+		Serial.print("There was error while reading: ");
+		Serial.println(testdata);
+	} else {
+		Serial.print("Successfully read data from the register. Data: ");
+		data = pga.getRecData();
+		for (int i = 0; i < 2; i++){
+			Serial.print(pga.binaryFormat(data[i],8));
+			Serial.print(" ");
+		}
+		Serial.println();
+	}
+	testdata = pga.readPGA309Register(0x06);
+	if (testdata != 0){
+		Serial.print("There was error while reading: ");
+		Serial.println(testdata);
+	} else {
+		Serial.print("Successfully read data from the register. Data: ");
+		data = pga.getRecData();
+		for (int i = 0; i < 2; i++){
+			Serial.print(pga.binaryFormat(data[i],8));
+			Serial.print(" ");
+		}
+		Serial.println();
+	}
+
 }
 
 void loop() {

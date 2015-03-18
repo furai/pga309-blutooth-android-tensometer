bee3d4c10424949dd9948340137ff986106693a1
Furai <furai@thd.vg>
2015-03-18 15:54:06 +0100

    [modified] values to ones that finally work

--- arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
+++ arduino_sketches/PGA309/testPin_I2C/testPin_I2C.ino
@@ -4,22 +4,24 @@
 
 #define PGA309ADDR 0x40
 #define ADS1110ADDR 0x49
-#define REG1 0x3BB5
-#define REG2 0xCD56
+#define REG1 0x0326
+#define REG2 0xE77B
 #define REG3 0x0500
-#define REG4 0xE709
+#define REG4 0xE700
 #define REG6 0x0600
+#define ADSREG B10001101
 
 void WriteToMultiRegisters(PGA309* _pga, int pregData[], int pregToWriteTo[], int size);
 void ReadFromMultRegisters(PGA309* _pga, int pregToReadFrom[], int size);
 void ReadADSData(ADS1110* _ads, byte data[]);
+void ReadErrorREG(PGA309* _pga);
 
 PGA309 pga(PGA309ADDR);
-// ADS1110 ads(ADS1110ADDR);
+ADS1110 ads(ADS1110ADDR);
 
 int regData[] = {REG1, REG2, REG3, REG4, REG6};
 int regToWriteTo[] = {0x01, 0x02, 0x03, 0x04, 0x06};
-int regToReadFrom[] = {0x01, 0x02, 	0x03, 0x04, 0x06, 0x08};
+int regToReadFrom[] = {0x01, 0x02, 0x03, 0x04, 0x06, 0x08};
 byte ADSData[3];
 
 void setup() {
@@ -32,11 +34,15 @@ void setup() {
 
 	WriteToMultiRegisters(&pga, regData, regToWriteTo, (sizeof(regData)/sizeof(int)));
 	ReadFromMultRegisters(&pga, regToReadFrom, (sizeof(regToReadFrom)/sizeof(int)));
+	Serial.print("Status of writing to the ADS internal register:");
+	Serial.println(ads.write(ADSREG));
 	delay(1000);
+
 }
 
 void loop() {
-	//ReadADSData(&ads, ADSData);
+	ReadADSData(&ads, ADSData);
+	ReadErrorREG(&pga);
 }
 
 //======================================================
@@ -74,6 +80,30 @@ void ReadFromMultRegisters(PGA309* _pga, int pregToReadFrom[], int size){
 	}
 		delay(5);
 	}
+}
+
+void ReadErrorREG(PGA309* _pga){
+	int testdata;
+	byte data[2];
+	
+	testdata = _pga->readPGA309Register(0x08);
+	if (testdata != 0){
+		Serial.print("There was error while reading data from register ");
+		Serial.print(0x08);
+		Serial.print(": ");
+		Serial.println(testdata);
+	} else {
+		Serial.print("Successfully read data from the register ");
+		Serial.print(0x08);
+		Serial.print(". Data: ");
+		_pga->getRecData(data);
+		for (int i = 1; i >= 0 ; i--){
+			Serial.print(_pga->binaryFormat(data[i],8));
+			Serial.print(" ");
+		}
+		Serial.println();
+	}
+		delay(5);
 }
 
 void ReadADSData(ADS1110* _ads, byte data[]){

21eeb551b9e61ebdce25de2c46df7a1323146f78
Furai <furai@thd.vg>
2015-03-20 15:30:39 +0100

    [modified] library files for EEPROM, no big changes, just syncing code

--- arduino_sketches/PGA309/testPin_I2C/EEPROM24C02.cpp
+++ arduino_sketches/PGA309/testPin_I2C/EEPROM24C02.cpp
@@ -1,5 +1,4 @@
 /*
-	EEPROM24C02.cpp - Library with helper functions.
-	Its sole purpose is to remove the functions from the main .ino file.
+	EEPROM24C02.cpp - Library for communicating with EEPROM24C02.
 	Created by Lucas Grzegorczyk, 20.03.2015
 */
--- arduino_sketches/PGA309/testPin_I2C/EEPROM24C02.h
+++ arduino_sketches/PGA309/testPin_I2C/EEPROM24C02.h
@@ -1,5 +1,11 @@
 /*
-	EEPROM24C02.h - Library with helper functions.
-	Its sole purpose is to remove the functions from the main .ino file.
+	EEPROM24C02.h - Library for communicating with EEPROM24C02.
 	Created by Lucas Grzegorczyk, 20.03.2015
 */
+#ifndef EEPROM24C02_h
+#define EEPROM24C02_h
+
+#include 
+
+#endif
+
